0621b8cef2da78f2189744cde263734012778f0e
anant <anant.lk.1997@gmail.com>
2018-09-08 19:07:18 +0530

    divided work among the cores

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
-let Jimp = require("jimp");
-let des = require("./des/des.js");
-const key = "10101010";
-let arr = [];
-let decrypt = [];
-function stringToHex(s) {
-  var r = "0x";
-  var hexes = new Array(
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f"
-  );
-  for (var i = 0; i < s.length; i++) {
-    r += hexes[s.charCodeAt(i) >> 4] + hexes[s.charCodeAt(i) & 0xf];
-  }
-  return r;
+const readImage = require("./readImage");
+const task = require("./task");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+let workers = [];
+let pos = 0;
+let encryptedData = [];
+let temp = [];
+if (cluster.isMaster) {
+  console.log(`Number of processors are ${numCPUs}`);
+  masterProcess();
+} else {
+  childProcess();
 }
 
-Jimp.read("download.jpeg")
-  .then(image => {
-    image.resize(50, 50);
-    for (let i = 0; i < image.bitmap.data.length; i++) {
-      let cipherText = des.des(key, image.bitmap.data[i].toString(), 1);
-      let str = stringToHex(cipherText);
-      image.bitmap.data[i] = parseInt(str, 16) % 255;
-    }
-    image.write("lena-small-bw.jpg");
-    return image.bitmap.data;
-  })
-  .catch(err => {
-    console.error(err);
+function childProcess() {
+  process.on("message", obj => {
+    console.log(
+      `Message recieved by child process Id ${process.pid} is ${
+        obj.data.pixels.length
+      }`
+    );
+    let encryptedPixels = task.encrypt(obj.data.pixels);
+    console.log(
+      `Process ${process.pid} recieves pixel data of length ${
+        encryptedPixels.length
+      }`
+    );
+    process.send({ id: obj.data.id, data: encryptedPixels });
   });
+}
+
+async function masterProcess() {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    console.log(`Forking process number ${i}`);
+    const worker = cluster.fork();
+    workers.push(worker);
+    worker.on("message", msg => {
+      console.log(
+        `Master with process id ${msg.id} recieves msg ${
+          msg.data.length
+        } from worker ${worker.process.pid}`
+      );
+      temp.push(msg);
+    });
+  }
+  let image = await readImage.read();
+  let pixelArray = Array.prototype.slice.call(image.bitmap.data, 0);
+  let numOfPixels = pixelArray.length;
+  console.log(numOfPixels);
+  workers.forEach((worker, index) => {
+    worker.send({
+      data: {
+        id: index + 1,
+        pixels: pixelArray.slice(
+          pos,
+          pos + parseInt(numOfPixels / workers.length)
+        )
+      }
+    });
+    pos = pos + parseInt(numOfPixels / workers.length);
+  });
+}
---
+++ task.js
@@ -0,0 +1,39 @@
+const des = require("./des/des.js");
+const key = "10101010";
+let arr = [];
+
+module.exports.encrypt = data => {
+  console.log("Encrypting the data");
+  for (let i = 0; i < data.length; i++) {
+    let cipherText = des.des(key, data[i].toString(), 1);
+    let str = stringToHex(cipherText);
+    arr.push(parseInt(str, 16) % 255);
+  }
+  return arr;
+};
+
+function stringToHex(s) {
+  var r = "0x";
+  var hexes = new Array(
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+  );
+  for (var i = 0; i < s.length; i++) {
+    r += hexes[s.charCodeAt(i) >> 4] + hexes[s.charCodeAt(i) & 0xf];
+  }
+  return r;
+}
